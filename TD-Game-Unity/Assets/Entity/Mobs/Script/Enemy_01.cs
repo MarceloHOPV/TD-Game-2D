64c37a571220ad12eb40c29f096660e009058a80
MarceloHOPV <marcelo.henrique@gec.inatel.br>
2026-09-09 23:48:12 -0300

    Initial commit
    feat: Add initial enemy, paths, and waypoints

---
+++ TD-Game-Unity/Assets/Entity/Mobs/Script/Enemy_01.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Enemy_01 : MonoBehaviour
+{
+    private enum EstadoInimigo
+    {
+        Andar
+    }
+
+    [SerializeField] private Transform pathContainer;
+    [SerializeField] private float velocidade = 2f;
+
+    private Transform[] waypoints;
+    private EstadoInimigo estadoAtual = EstadoInimigo.Andar;
+    private int waypointAtual = 0;
+
+    void Start()
+    {
+        waypoints = new Transform[pathContainer.childCount];
+        for (int i = 0; i < pathContainer.childCount; i++)
+        {
+            waypoints[i] = pathContainer.GetChild(i);
+        }
+    }
+
+    void Update()
+    {
+        switch (estadoAtual)
+        {
+            case EstadoInimigo.Andar:
+                Andar();
+                break;
+        }
+    }
+
+    private void Andar()
+    {
+        if (waypointAtual >= waypoints.Length)
+            return;
+
+        Transform destino = waypoints[waypointAtual];
+        transform.position = Vector3.MoveTowards(transform.position, destino.position, velocidade * Time.deltaTime);
+
+        if (transform.position == destino.position)
+        {
+            waypointAtual++;
+        }
+    }
+}
